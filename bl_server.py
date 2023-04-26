7958873d8504affbff60e5d2a204c54b92e19b42
Petre-Dominic <anticaedward@gmail.com>
2023-04-27 00:05:53 +0300

    "Create basic BL service for communication w/ IO"

---
+++ bl_server.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
+import os
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+URL = 'http://io_service:5000/save_session'
+
+print("Authenthication server connected to database! :)")
+
+@app.route("/save_session", methods=["POST"])
+def register():
+    req = request.json
+    result = requests.post(url = URL, json = req)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.1")
