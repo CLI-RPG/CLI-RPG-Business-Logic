cbf2d2c6bc34b6c04aa57a8c6ec9cb0a863f3595
NethDR <mihai.udubasa@yahoo.com>
2023-05-19 12:54:12 +0300

    refactor and add jwt handling

--- bl_server.py
+++ bl_server.py
@@ -5,7 +5,8 @@ import os
 import requests
 import http
 import json
-
+import jwt
+from functools import wraps
 
 EMPTY = 0
 WALL = 1
@@ -15,84 +16,59 @@ PLAYER = 4
 PLAYER_ON_SHOP = 5
 PLAYER_ON_ENEMY = 6
 
+STATE_IDLE = 0
+STATE_SHOP = 1
+STATE_FIGHT = 2
+
 app = Flask(__name__)
 CORS(app)
 
+JWT_SECRET = "secret"
+
 IOSERVICE_BASE_URL = "http://io_service:5000"
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
 
-@app.route("/act/<session_id>", methods=["POST"])
-def act(session_id):
-    action = request.json
-    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=f"This will be the response you get after the action {action} for {session_id=}")
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+            uid = data["user_id"]
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users context to the routes
+        return  f(uid, *args, **kwargs)
 
-@app.route("/player_stats/<session_id>", methods=["GET"])
-def stats(session_id):
-    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps({"health":100, "attack":50, "shield": 20, "gold": 67}))
+    return decorated
 
-@app.route("/map/<session_id>", methods=["GET"])
-def map(session_id):
-#   e # _ _ $
-#   _ # e _ _
-#   _ _ P _ _
-#   _ _ # # #
-#   _ _ _ e _
+class GameState:
 
-
-
-    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps(
-        [
-            ENEMY,  WALL,   EMPTY,  EMPTY,  SHOP,
-            EMPTY,  WALL,   ENEMY,  EMPTY,  EMPTY,
-            EMPTY,  EMPTY,  PLAYER, EMPTY,  EMPTY,
-            EMPTY,  EMPTY,  WALL,   WALL,   WALL,
-            EMPTY,  EMPTY,  EMPTY,  ENEMY,  EMPTY
-        ]
-    ))
-
-@app.route("/scenario/<session_id>", methods=["GET"])
-def scenario(session_id):
-    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps(f"This will be the scenario description for {session_id=}"))
-
-@app.route("/actions/<session_id>", methods=["GET"])
-def actions(session_id):
-    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps({1: "up", 2: "down", 3: "left", 4: "right"}))
-
-@app.route("/new_game/<user_id>", methods=["GET"]) # TODO decide if get is the best method
-def newgame(user_id):
-    req = request.json
-    name = req["name"]
-    level = req["level"]
-    scenario = req["scenario"]
-    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps(f"id of the new game named {name} of level {level} on scenario {scenario}"))
-
-@app.route("/saved_games/<user_id>", methods=["GET"])
-def savedgames(user_id):
-    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps(["name1", "name2", "etc..."]))
-
-@app.route("/load_game/<user_id>/<game_name>", methods=["GET"]) # TODO decide if get is the best method
-def loadgame(user_id, game_name):
-    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps("id of the session"))
-
-def get_actions(gameMap):
-    if PLAYER in gameMap:
-        return {
+    STATE_TO_ACTION_TEXT = {
+        STATE_IDLE : {
             1 : "up",
             2 : "down",
             3 : "left",
             4 : "right"
-        }
-    elif PLAYER_ON_ENEMY in gameMap:
-        return {
+        },
+        STATE_FIGHT : {
             1 : "flee up",
             2 : "flee down",
             3 : "flee left",
             4 : "flee right",
             5 : "attack",
             6 : "defend"
-        }
-    elif PLAYER_ON_SHOP in gameMap:
-        return {
+        },
+        STATE_SHOP : {
             1 : "up",
             2 : "down",
             3 : "left",
@@ -101,19 +77,172 @@ def get_actions(gameMap):
             6 : "upgrade shield <y gold>",
             7:  "heal <z gold>"
         }
+    }
 
-@app.route("/session_data/<session_id>", methods=["GET"])
-def get_everything(session_id):
+    def __init__(self,
+                 user_id,
+                 health,
+                 attack,
+                 shield,
+                 level,
+                 game_map,
+                 money,
+                 scenario_id,
+                 current_enemy_hp) -> None:
+        self.user_id = user_id
+        self.health = health
+        self.attack = attack
+        self.shield = shield
+        self.level = level
+        self.game_map = game_map
+        self.money = money
+        self.scenario_id = scenario_id
+        self.current_enemy_hp = current_enemy_hp
+        if PLAYER in game_map:
+            self.state = STATE_IDLE
+        elif PLAYER_ON_ENEMY in game_map:
+            self.state = STATE_FIGHT
+        elif PLAYER_ON_SHOP in game_map:
+            self.state = STATE_SHOP
+        self.action_text = GameState.STATE_TO_ACTION_TEXT[self.state]
+
+
+
+    @staticmethod
+    def fromJSON(data) -> 'GameState':
+        return GameState(
+            data.get('userID'),
+            data.get('health'),
+            data.get('attackPwr'),
+            data.get('shield'),
+            data.get('level'),
+            data.get('map'),
+            data.get('money'),
+            data.get('scenarioID'),
+            data.get('currentEnemyHP')
+        )
+
+    def toJSON(self):
+        return json.dumps({
+            "userID": self.user_id,
+            "health" : self.health,
+            "attackPwr" : self.attack,
+            "shield" : self.shield,
+            "level" : self.level,
+            "money" : self.money,
+            "map" : self.game_map,
+            "scenarioID" : self.scenario_id,
+            "currentEnemyHP" : self.current_enemy_hp
+        })
+
+dungeon_side = {
+    1 : 5,
+    2 : 7,
+    3 : 9
+}
+
+
+@app.route("/act/<session_id>/<int:action_id>", methods=["POST"])
+@token_required
+def act(uid, session_id, action_id):
+
     result = requests.get(IOSERVICE_BASE_URL + "/get_session/" + session_id)
 
     if (not result.ok):
         return Response(status=result.status_code)
 
-    js = result.json()
+    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED)
 
-    js["actions"] = get_actions(js["map"])
 
-    return Response(status=result.status_code, response=json.dumps(js))
+@app.route("/new_game", methods=["POST"]) # TODO decide if POSt is the best method
+@token_required
+def newgame(uid):
+    req = request.json
+    if req is None:
+        return Response(status=http.HTTPStatus.BAD_REQUEST)
+    name = req["name"]
+    level = req["level"]
+    scenario = req["scenario"]
+    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps(f"id of the new game named {name} of level {level} on scenario {scenario}"))
+
+@app.route("/saved_games", methods=["GET"])
+@token_required
+def savedgames(user_id):
+    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps(["name1", "name2", "etc..."]))
+
+# @app.route("/load_game/<user_id>/<game_name>", methods=["GET"]) # TODO decide if get is the best method
+# def loadgame(user_id, game_name):
+#     return Response(status=http.HTTPStatus.NOT_IMPLEMENTED, response=json.dumps("id of the session"))
+
+
+@app.route("/session_data/<session_id>", methods=["GET"])
+@token_required
+def get_everything(uid, session_id):
+    result = requests.get(IOSERVICE_BASE_URL + "/get_session/" + session_id)
+
+    if (not result.ok):
+        return Response(status=result.status_code)
+
+    game = GameState.fromJSON(result.json())
+
+    if game.user_id != uid:
+        return Response(status=http.HTTPStatus.FORBIDDEN)
+
+    return Response(status=result.status_code, response=game.toJSON())
+
+def generate_map(level, seed=None):
+    side = dungeon_side[level]
+    import random
+    if seed is None:
+        import time
+        seed = time.time()
+    rng = random.Random(seed)
+
+    occupied = set()
+    game_map = [[EMPTY for _ in range(side)] for _ in range(side)]
+
+    def random_pos():
+        pos = (rng.randrange(0, side), rng.randrange(0, side))
+        while pos in occupied:
+            pos = (rng.randrange(0, side), rng.randrange(0, side))
+        return pos
+
+
+
+    player_start_x, player_start_y = player_start = random_pos()
+    game_map[player_start_x][player_start_y] = PLAYER
+    occupied.add(player_start)
+
+    shop_x, shop_y = shop = random_pos()
+    game_map[shop_x][shop_y] = SHOP
+
+
+
+def render(map):
+    from math import sqrt
+    tile_to_char = {
+        EMPTY: "_",
+        PLAYER: "P",
+        WALL: "#",
+        SHOP: "$",
+        ENEMY: "E"
+    }
+
+    ml = []
+
+    n = int(sqrt(len(m)))
+
+    while m:
+        ml.append(m[:n])
+        m = m[n:]
+
+    ms = ["┌" + (n * 2 + 1) * "─" + "┐", *["│ " + " ".join([tile_to_char[t] for t in r]) + " │" for r in ml],"└" + (n * 2 + 1) * "─" + "┘"]
+
+    return "\n".join(ms)
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
